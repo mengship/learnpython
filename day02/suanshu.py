cebd071a25f789d0e47f967f113bd7f50f702057
wangyuqi <wangdongchen@flashexpress.com>
2022-07-09 17:42:56 +0800

    算术 commit

--- day02/suanshu.py
+++ day02/suanshu.py
@@ -2,9 +2,9 @@
 使用input()函数获取键盘输入(字符串)
 使用int()函数将输入的字符串转换成整数
 使用print()函数输出带占位符的字符串
-
+学习的链接: https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/02.%E8%AF%AD%E8%A8%80%E5%85%83%E7%B4%A0.md
 Version: 0.1
-Author: 骆昊
+Author: 王昱棋
 """
 a = int(input('a = '))
 b = int(input('b = '))
